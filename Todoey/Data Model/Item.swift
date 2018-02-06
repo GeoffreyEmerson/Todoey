0fce2ee92123bb0cb9e75985335b6beee7182f5a
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2018-02-06 15:48:41 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Geoffrey Emerson on 1/24/18.
+//  Copyright © 2018 Geoffrey Emerson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
